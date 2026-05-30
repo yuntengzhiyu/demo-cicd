66bb694bb12777ad578c1ae033f557aa0387828c
云腾致雨 <shenmafuyun213@163.com>
2026-05-30 15:17:46 +0800

    test cicd

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(@RequestParam(defaultValue = "World") String name) {
-        return "Hello, " + name + "!";
+        return "Hello, " + name + "! v2";
     }
 
     @GetMapping("/health")

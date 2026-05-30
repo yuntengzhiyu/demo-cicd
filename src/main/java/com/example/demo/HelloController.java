718298150f39441fbdf40a227469fb5d983191c7
云腾致雨 <shenmafuyun213@163.com>
2026-05-30 21:21:28 +0800

    test: trigger jenkins

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(@RequestParam(defaultValue = "World") String name) {
-        return "Hello, " + name + "! v9";
+        return "Hello, " + name + "! v10";
     }
 
     @GetMapping("/health")
